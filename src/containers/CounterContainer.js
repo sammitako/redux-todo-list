62ad14e070f456da8f6ea3be1e2a9f8a28d9281c
hansaempark <hansaempark@sunssc.com>
2022-03-22 16:57:09 +0900

    useSelector Optimization

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -5,10 +5,26 @@ import { decrease, increase, setDiff } from "../moduels/counrer";
 
 const CounterContainer = () => {
   // state = store.getState(), 리덕스의 현재 상태
-  const { number, diff } = useSelector((state) => ({
-    number: state.counter.number,
-    diff: state.counter.diff,
-  }));
+  // const { number, diff } = useSelector((state) => ({
+  //   number: state.counter.number,
+  //   diff: state.counter.diff,
+  // }));
+
+  // useSelector 최적화 (1) - useSelector 따로 써줌
+  // const number = useSelector((state) => state.counter.number);
+  // const diff = useSelector((state) => state.counter.diff);
+
+  // useSelector 최적화 (2) - equalityFn: 이전 상태와 다음 상태를 비교하는 함수
+  const { number, diff } = useSelector(
+    (state) => ({
+      number: state.counter.number,
+      diff: state.counter.diff,
+    }),
+    (left, right) => {
+      return left.diff === right.diff && left.number === right.number;
+    }
+  );
+
   const dispatch = useDispatch();
 
   const onIncrease = () => dispatch(increase());
